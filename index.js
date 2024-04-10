47a8f75799b33efea9d3ad9baacd0d400979f953
RileyHDeakin <109202681+RileyHDeakin@users.noreply.github.com>
2024-04-11 09:20:46 +1000

    Updating documentation in index.js

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const port = 3000;
 //mounting to the static file using a path to index.html
 app.use(express.static('public'));
 
+// Route for handling GET requests to the root endpoint '/'
 app.get('/', (req, res) => {
+// Sending index.html file located in the 'public' directory
   res.sendFile(__dirname + '/public/index.html');
 });
 
@@ -16,11 +18,11 @@ app.get('/', (req, res) => {
 app.get('/addition', (req, resp)=>{
     //check if query paratmers num1 and num2 are valid
     if(!req.query.num1 || !req.query.num2) {
+       // Sending a 400 Bad Request status with an error message if querys are invalid
         resp.status(400).send(`<h1>Invalid numbers or numbers don't exist</h1>`);
     }
-    //gets the num1 queary parameter and convert to float
+    // Parsing num1 and num2 from query parameters to float
     const num1 = parseFloat(req.query.num1);
-    //gets the num2 queary parameter and convert to float
     const num2 = parseFloat(req.query.num2);
 
     //add num1 and num2 and send result back to client as html
@@ -30,46 +32,53 @@ app.get('/addition', (req, resp)=>{
 
 //setup route for handling GET requests to the /subtraction endpoint
 app.get('/subtraction', (req, resp)=>{
+  // Checking if query parameters num1 and num2 are valid
     if(!req.query.num1 || !req.query.num2) {
+      // Sending a 400 Bad Request status with an error message if querys are invalid
         resp.status(400).send(`<h1>Invalid numbers or numbers don't exist</h1>`);
     }
-
+   //Parsing num1 and num2 from query parameters to float
     const num1 = parseFloat(req.query.num1);
     const num2 = parseFloat(req.query.num2);
 
+    // Subtracting num2 from num1 and sending result back to client as HTML
     const result = num1 - num2;
     resp.send(`<h1>${result}</h1>`);
 });
 
-//setup route for handling GET requests to the /multiplaction endpoint
+// Setup route for handling GET requests to the '/multiplication' endpoint
 app.get('/multiplication', (req, resp)=>{
-
+    // Checking if query parameters num1 and num2 are valid
     if(!req.query.num1 || !req.query.num2) {
+      // Sending a 400 Bad Request status with an error message if querys are invalid
         resp.status(400).send(`<h1>Invalid numbers or numbers don't exist</h1>`);
     }
-
+    // Parsing num1 and num2 from query parameters to float
     const num1 = parseFloat(req.query.num1);
     const num2 = parseFloat(req.query.num2);
 
+    // Multiplying num1 and num2 and sending result back to client as HTML
     const result = num1 * num2;
     resp.send(`<h1>${result}</h1>`);
 });
 
-//setup route for handling GET requests to the /division endpoint
+// Setup route for handling GET requests to the '/division' endpoint
 app.get('/division', (req, resp)=>{
-
+    // Checking if query parameters num1 and num2 are valid
     if(!req.query.num1 || !req.query.num2) {
+        // Sending a 400 Bad Request status with an error message
         resp.status(400).send(`<h1>Invalid numbers or numbers don't exist</h1>`);
     }
-
+    // Parsing num1 and num2 from query parameters to float
     const num1 = parseFloat(req.query.num1);
     const num2 = parseFloat(req.query.num2);
 
+    // Dividing num1 by num2 and sending result back to client as HTML
     const result = num1 / num2;
     resp.send(`<h1>${result}</h1>`);
 });
 
-//configures server to listen to the set port number then logs message
+// Configuring server to listen to the set port number then logging a message
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
